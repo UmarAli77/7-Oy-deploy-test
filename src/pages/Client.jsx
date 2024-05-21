64303cfc34c8735eaec7c985ea1eb7591b1be0df
MUHAMMAD <umarasqaraliov@gmail.com>
2024-05-21 18:03:42 +0500

    change all

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -8,6 +8,7 @@ import { useGetData } from '../hook/getHook';
 function Client() {
     const navigate = useNavigate();
     const cart = useGetData('categories');
+    const product = useGetData('products');
     useEffect(() => {
         const token = localStorage.getItem('token');
         if(!token) {
@@ -31,16 +32,29 @@ function Client() {
         }
     }
     return (
-    <div className='grid grid-cols-3 justify-evenly'>
-        {
-            cart && cart.map(el => 
-                <div className='w-[230px] text-center shadow-lg p-4 hover:shadow-xl'>
-                    <img src={el.image} alt="" />
-                    <h2>{el.name}</h2>
-                    <button onClick={() => handleDelete(el._id)} className='bg-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded'>Delete</button>
-                </div>
-            )
-        }
+    <div>
+        <div className='grid grid-cols-3 justify-evenly'>
+            {
+                cart && cart.map(el => 
+                    <div className='w-[230px] text-center shadow-lg p-4 hover:shadow-xl'>
+                        <img src={el.image} alt="" />
+                        <h2>{el.name}</h2>
+                        <button onClick={() => handleDelete(el._id)} className='bg-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded'>Delete</button>
+                    </div>
+                )
+            }
+        </div>
+        <div className='grid grid-cols-3 justify-evenly'>
+            {
+                product && product.map(el => 
+                    <div className='w-[230px] text-center shadow-lg p-4 hover:shadow-xl'>
+                        <img src={el.image} alt="" />
+                        <h2>{el.name}</h2>
+                        <button onClick={() => handleDelete(el._id)} className='bg-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded'>Delete</button>
+                    </div>
+                )
+            }
+        </div>
     </div>
   )
 }
